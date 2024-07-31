36d1a51a5aff1e999029a6f77255ff4928bc0a4f
Mayank Tyagi <mayank.tyagi@devstringx.com>
2024-07-31 18:11:28 +0530

    test

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,30 +1,38 @@
-const cypress = require("cypress")
+const cypress = require("cypress");
 const { defineConfig } = require("cypress");
-const { tagify } = require('cypress-tags');
-require('cypress-mochawesome-reporter/plugin');
+const { tagify } = require("cypress-tags");
+const path = require('path');
+require("cypress-mochawesome-reporter/plugin");
 
 module.exports = defineConfig({
-    reporter: 'cypress-mochawesome-reporter',
-   
+  reporter: "cypress-mochawesome-reporter",
+
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      on('file:preprocessor', tagify(config));
+      require("cypress-mochawesome-reporter/plugin")(on);
+      on("file:preprocessor", tagify(config));
       // implement node event listeners here
     },
-    specPattern:'cypress/Integration/API/*.spec.js',
+    specPattern: "cypress/Integration/API/*.spec.js",
     screenshotOnRunFailure: true,
-    screenshotsFolder: 'cypress/failures/screenshots'
+    screenshotsFolder: "cypress/failures/screenshots",
   },
+  reporter: "cypress-mochawesome-reporter",
   env: {
-    CYPRESS_INCLUDE_TAGS: 'smoke,sanity,regression',
-    base_url: 'https://simple-books-api.glitch.me',
-    authEndPoint : '/api-clients',
-    listOfBooks : '/books',
-    singleBook : '/books/1',
-    submitOrder : '/orders',
-    getAllOrders : '/orders',
-    updateOrder : '/orders/',
-    deleteOrder : '/orders/'
+    mochawesomeReporterOptions: {
+      reportDir: path.join(__dirname, "cypress/reports/html/.jsons"), // Specify report output directory
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+    CYPRESS_INCLUDE_TAGS: "smoke,sanity,regression",
+    base_url: "https://simple-books-api.glitch.me",
+    authEndPoint: "/api-clients",
+    listOfBooks: "/books",
+    singleBook: "/books/1",
+    submitOrder: "/orders",
+    getAllOrders: "/orders",
+    updateOrder: "/orders/",
+    deleteOrder: "/orders/",
   },
 });
