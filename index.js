f0e7cdb9f44f044461fe73eda94904125c1c8701
Mayank Tyagi <mayank.tyagi@devstringx.com>
2024-08-01 12:34:33 +0530

    updated index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import fetch from 'node-fetch';
 
+const yxz='';
+const buildNum = process.env.CIRCLE_BUILD_NUM;
+const vcsType = process.env.CIRCLE_PROJECT_VCS_TYPE;
+const username = process.env.CIRCLE_PROJECT_USERNAME;
+const project = process.env.CIRCLE_PROJECT_REPONAME;
+
 async function sendToGoogleChat() {
     const url = "https://chat.googleapis.com/v1/spaces/AAAA8mmXBAg/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=SWdadv404w_RiJ1fdvBYN1NyaIUmj-NjtpLpIdFj5vk"
     const message = {
-      text: `Build Successful!! Get the report from the artifacts.`
+      text: `Build Successful!! Get the report from the artifacts.... BUild Number: ${buildNum}, username: ${username}, Repo-Name: ${project} ,version control system: ${vcsType}`
     };
     const res = await fetch(url, {
       method: "POST",
